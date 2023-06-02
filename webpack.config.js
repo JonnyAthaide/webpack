e38bd23b0d18b661201568bd93d867645e8250b4
JonnyAthaide <jonny.athaide@hotmail.com>
2023-06-02 13:56:41 -0300

    - webpack jsx

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 // use: {
