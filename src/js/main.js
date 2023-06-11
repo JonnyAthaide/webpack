5a6f96ec4cc56bb5826d993e65af81e9d104191e
JonnyAthaide <jonny.athaide@hotmail.com>
2023-06-11 02:58:16 -0300

    partial html  grid template area

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
 import '../scss/main.scss'
 import img from '../assets/webpack.png'
 
-import imagemWebP from '../assets/webpack.png'
+// import imagemWebP from '../assets/webpack.png'
 
-// Use a imagemWebP como quiser
+// // Use a imagemWebP como quiser
 
 
-const textComponent = ()=> {
-    const elH1 = document.createElement('h1')
-    elH1.innerHTML = "Olá server Mundo!"
-    elH1.classList.add('title')
-    return elH1
-}
+// const textComponent = ()=> {
+//     const elH1 = document.createElement('h1')
+//     elH1.innerHTML = "Olá server Mundo!"
+//     elH1.classList.add('title')
+//     return elH1
+// }
 
-const imgComponent = ()=> {
-    const elImg = new Image(300, 150)
-    elImg.src = imagemWebP
-    return elImg
-}
+// const imgComponent = ()=> {
+//     const elImg = new Image(300, 150)
+//     elImg.src = imagemWebP
+//     return elImg
+// }
 
-document.body.appendChild(textComponent())
-document.body.appendChild(imgComponent())
+// document.body.appendChild(textComponent())
+// document.body.appendChild(imgComponent())
